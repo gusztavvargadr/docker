ecb10ba9c28afeb96d6efb9c3f8a36663c5ac6e7
Gusztáv Varga <human@gusztavvargadr.me>
2018-12-02 01:53:03 +0100

    Fix engine and swarm provisioning #7

--- samples/images/docker-machine-configuration/templates/server.sh
+++ samples/images/docker-machine-configuration/templates/server.sh
@@ -4,7 +4,7 @@ cp machines/$1/ca.pem /etc/docker
 cp machines/$1/server.pem /etc/docker
 cp machines/$1/server-key.pem /etc/docker
 
-sed -i -- 's/ExecStart=\/usr\/bin\/dockerd -H fd:\/\//ExecStart=\/usr\/bin\/dockerd/g' /lib/systemd/system/docker.service
+sed -i -- 's/ExecStart=\/usr\/bin\/dockerd -H unix:\/\//ExecStart=\/usr\/bin\/dockerd/g' /lib/systemd/system/docker.service
 cp machines/$1/server.linux.json /etc/docker/daemon.json
 
 systemctl daemon-reload
--- samples/vagrant.rb
+++ samples/vagrant.rb
@@ -5,21 +5,11 @@ require "#{directory}/../lib/gusztavvargadr/vagrant/samples/vagrant"
 VagrantMachine.defaults_include(
   'synced_folders' => {
     '/vagrant' => {
-      'source' => '.vagrant',
       'disabled' => true,
     },
     '/data/docker' => {
       'source' => '.docker',
       'create' => true,
     },
-  },
-  'providers' => {
-    'virtualbox' => {},
-    'hyperv' => {},
   }
-)
-
-VagrantProvider.defaults_include(
-  'memory' => 2048,
-  'cpus' => 2
 )
